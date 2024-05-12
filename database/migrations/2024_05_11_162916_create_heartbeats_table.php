79b859a2bf104f36fe175e1d259c61d8d5ed31f8
Nick Galvez <nicholas.galvez@gmail.com>
2024-05-12 13:44:00 -0700

    Update migration for heartbeats

--- database/migrations/2024_05_11_162916_create_heartbeats_table.php
+++ database/migrations/2024_05_11_162916_create_heartbeats_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('heartbeats', function (Blueprint $table) {
             $table->id();
             $table->string('device_id');
-            $table->string('status')->nullable();
+            $table->string('status');
             $table->string('ip_address', 45)->nullable();
             $table->timestamps();
         });
